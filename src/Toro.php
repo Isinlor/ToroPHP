f273fc7c8d9d9b27bc390fd781ba80fa409ea1ab
Isinlor <isinlor@gmail.com>
2013-09-19 17:55:59 +0200

    Merge ToroUtil from anothergituser

--- src/Toro.php
+++ src/Toro.php
@@ -2,11 +2,17 @@
 
 class Toro
 {
-    public static function serve($routes)
-    {
-        ToroHook::fire('before_request', compact('routes'));
+    // used in ToroUtil::url_for to resolve handlers urls
+    private static $_routes;
 
-        $request_method = strtolower($_SERVER['REQUEST_METHOD']);
+    // and the method to return the routes
+    public static function getRoutes()
+    {
+        return self::$_routes;
+    }
+
+    private static function get_path_info()
+    {
         $path_info = '/';
         if (!empty($_SERVER['PATH_INFO'])) {
             $path_info = $_SERVER['PATH_INFO'];
@@ -26,6 +32,28 @@ class Toro
                 }
             }
         }
+        return $path_info;
+    }
+
+    // route convenience tokens
+    public static function getTokens()
+    {
+        return array(
+            ':string' => '([a-zA-Z]+)',
+            ':number' => '([0-9]+)',
+            ':alpha' => '([a-zA-Z0-9-_]+)'
+        );
+    }
+
+    public static function serve($routes)
+    {
+        self::$_routes = $routes;
+
+        ToroHook::fire('before_request', compact('routes'));
+
+        $request_method = strtolower($_SERVER['REQUEST_METHOD']);
+
+        $path_info = self::get_path_info();
 
         $discovered_handler = null;
         $regex_matches = array();
@@ -34,11 +62,9 @@ class Toro
             $discovered_handler = $routes[$path_info];
         }
         else if ($routes) {
-            $tokens = array(
-                ':string' => '([a-zA-Z]+)',
-                ':number' => '([0-9]+)',
-                ':alpha'  => '([a-zA-Z0-9-_]+)'
-            );
+
+            $tokens = self::getTokens();
+
             foreach ($routes as $pattern => $handler_name) {
                 $pattern = strtr($pattern, $tokens);
                 if (preg_match('#^/?' . $pattern . '/?$#', $path_info, $matches)) {
@@ -55,12 +81,7 @@ class Toro
             $handler_instance = new $discovered_handler();
 
             if (self::is_xhr_request() && method_exists($discovered_handler, $request_method . '_xhr')) {
-                header('Content-type: application/json');
-                header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-                header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
-                header('Cache-Control: no-store, no-cache, must-revalidate');
-                header('Cache-Control: post-check=0, pre-check=0', false);
-                header('Pragma: no-cache');
+                self::set_xhr_response_headers();
                 $request_method .= '_xhr';
             }
 
@@ -83,6 +104,16 @@ class Toro
     private static function is_xhr_request()
     {
         return isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest';
+    }
+
+    private static function set_xhr_response_headers()
+    {
+        header('Content-type: application/json');
+        header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+        header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+        header('Cache-Control: no-store, no-cache, must-revalidate');
+        header('Cache-Control: post-check=0, pre-check=0', false);
+        header('Pragma: no-cache');
     }
 }
 
@@ -117,5 +148,106 @@ class ToroHook
             self::$instance = new ToroHook();
         }
         return self::$instance;
+    }
+}
+
+class ToroUtil
+{
+
+    /* only used for testing */
+    public static $routes = array();
+
+    /*
+    * Tries to return the url for a handler if the handler exists.
+    * If the route of this handler was defined using parameters
+    * then an array with the parameters in the same order is expected.
+    *
+    * example:
+    *
+    * Toro::serve(array(
+    * "/" => "IndexHandler",
+    * "/hello/:alpha" => "HelloHandler",
+    * "/test/this" => "TestHandler",
+    * ));
+    *
+    * ToroUtil::url_for("IndexHandler") would return "/"
+    * ToroUtil::url_for("HelloHandler", array("test")) would return "/hello/test"
+    *
+    * but
+    *
+    * ToroUtil::url_for("HelloHandler") will not return because it's missing the
+    * :alpha parameter
+    *
+    * and
+    *
+    * ToroUtil::url_for("TestHandler") would return "/test/this"
+    *
+    * This is because routes can change so it is better
+    * to define them only in one place (DRY)
+    *
+    */
+
+    public static function url_for($handler, $params = array())
+    {
+        $tokens = Toro::getTokens();
+        $routes = count(self::$routes) > 0 ? self::$routes : Toro::getRoutes();
+
+        foreach ($routes as $pattern => $handler_name)
+        {
+            if ($handler_name == $handler)
+            {
+
+                /* convert the tokens like :string to regex like ([a-zA-Z]+) */
+                $pattern = strtr($pattern, $tokens);
+                /* find all the regex parameters in the route pattern */
+                preg_match_all('#\([^\(\)]+\)#', $pattern, $regs);
+                /*
+                * Replaces all the regex patterns in the defined route for this handler
+                * with their parameter counterparts
+                *
+                * If the defined route for this handler is something like /hello/:alpha-:alpha
+                * and the reverse route is required, having specified an array of parameters
+                * like array("this", "that") the resulting url should be /hello/this-that
+                *
+                * Now, :alpha get's transformed to ([a-zA-Z0-9-_]+)
+                * and then it is replaced with the parameter in the array
+                * first occurence of the regex is replaced with the first parameter
+                * the second occurence with the second parameter and so on..
+                *
+                * Finally, the resulting string is returned if the original pattern
+                * for the handler matches it.
+                */
+
+                if (isset($regs[0]))
+                {
+                    $url = $pattern;
+                    for ($i=0; $i < count($regs[0]); $i++) {
+                        $pat = $regs[0][$i];
+                        if (isset($params[$i]))
+                        {
+                            $par = $params[$i];
+                            $pos = strpos($url, $pat);
+                            if ($pos != false)
+                            {
+                                $url = substr_replace($url, $par, $pos, strlen($pat));
+                            }
+                        }
+
+                    }
+                }
+                
+
+                /*
+                * test that the route pattern matches the resulting url
+                * to validate that the correct number and type of parameters
+                * were received
+                */
+                
+                if (preg_match('#^/?' . $pattern . '/?$#', $url))
+                {
+                    return $url;
+                }
+            }
+        }
     }
 }
